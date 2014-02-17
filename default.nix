a61645923decb318451e85a019890068fcec6c74
Oliver Charles <ollie@ocharles.org.uk>
2014-02-17 12:55:10 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,7 @@
-{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
-with import <nixpkgs> {};
-let
-  inherit (haskellPackages) cabal cabalInstall_1_18_0_2 snap;
-in cabal.mkDerivation (self: {
+{ cabal, snap }:
+cabal.mkDerivation (self: {
   pname = "snap-cors";
   version = "1.0.0";
   src = ./.;
   buildDepends = [ snap ];
-  buildTools = [ cabalInstall_1_18_0_2 ];
 })
