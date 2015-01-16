76e632f594bd375d702d07e46b4ece2f511ea199
Oliver Charles <ollie@ocharles.org.uk>
2015-01-16 20:09:42 +0000

    Regenerate nix expressions

--- default.nix
+++ default.nix
@@ -1,7 +1,16 @@
-{ cabal, networkUri, snap }:
-cabal.mkDerivation (self: {
+{ mkDerivation, attoparsec, base, bytestring, case-insensitive
+, hashable, network, network-uri, snap, stdenv, text, transformers
+, unordered-containers
+}:
+mkDerivation {
   pname = "snap-cors";
-  version = "1.0.0";
+  version = "1.2.6";
   src = ./.;
-  buildDepends = [ networkUri snap ];
-})
+  buildDepends = [
+    attoparsec base bytestring case-insensitive hashable network
+    network-uri snap text transformers unordered-containers
+  ];
+  homepage = "http://github.com/ocharles/snap-cors";
+  description = "Add CORS headers to Snap applications";
+  license = stdenv.lib.licenses.bsd3;
+}
